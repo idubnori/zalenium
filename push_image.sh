0b3423c277d293a555cd77b8b3999ea27548ad80
idubnori <i.dub.nori@gmail.com>
2019-11-17 22:04:10 +0900

    Modfied docker commands, temporal

--- push_image.sh
+++ push_image.sh
@@ -15,9 +15,12 @@ cat scm-source.json
 	echo "Building image..."
 	mvn clean package -Pbuild-docker-image -DskipTests=true
 	echo "Starting to push Zalenium image..."
-	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker images
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     echo "Logged in to docker with user '${DOCKER_USERNAME}'"
     echo "docker tag and docker push using TRAVIS_TAG=${TRAVIS_TAG}"
+    docker tag zalenium:3.141.59v-SNAPSHOT idubnori/zalenium:build-${TRAVIS_BUILD_NUMBER}
+    docker push idubnori/zalenium:build-${TRAVIS_BUILD_NUMBER} | tee docker_push.log
     # docker tag zalenium:${TRAVIS_TAG} dosel/zalenium:${TRAVIS_TAG}
     # docker push dosel/zalenium:${TRAVIS_TAG} | tee docker_push.log
     # if [[ "${TRAVIS_TAG}" == "3."* ]]; then
